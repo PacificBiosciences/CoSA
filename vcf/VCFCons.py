6530c912972f8cdfb5c0813a0bbd8559c7e42c37
zkronenberg <zkronenberg@pacificbiosciences.com>
2021-07-22 08:48:01 -0600

    only for bcftools

--- vcf/VCFCons.py
+++ vcf/VCFCons.py
@@ -143,10 +143,10 @@ def genVCFcons(ref_fasta, depth_file, vcf_input, prefix, newid,
             lastDelCov = total_cov
         else:
             # the variant (SNV/Insertion) starts before the last deletion ends, it's contained
-            if v.POS < lastDelEnd:
+            if (v.POS < lastDelEnd) and (vcf_type == 'bcftools'):
                 # keep the maximum coverage, i.e. the spanning read coverage.
                 total_cov = max(total_cov, lastDelCov)
-        # recalculate frequency        
+        # recalculate frequency
         alt_freq = alt_count * 1. / total_cov
 
         if total_cov < min_coverage:
